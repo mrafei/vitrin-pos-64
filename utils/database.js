b9268c82592954d30066870bbbe041ac27bf5bc1
mrafei <m.rafei96@gmail.com>
2020-06-22 17:03:44 +0430

    Add knex for orm

--- utils/database.js
+++ utils/database.js
@@ -1,26 +1,21 @@
-const sqlite3 = require('sqlite3');
-
-const db = new sqlite3.Database('./database.db');
-
-const createTestDataBase = () => db.serialize(function () {
-  db.run("CREATE TABLE Products (name, barcode, quantity)");
-
-  db.run("INSERT INTO Products VALUES (?, ?, ?)", ['product001', 'xxxxx', 20]);
-  db.run("INSERT INTO Products VALUES (?, ?, ?)", ['product002', 'xxxxx', 40]);
-  db.run("INSERT INTO Products VALUES (?, ?, ?)", ['product003', 'xxxxx', 60]);
-
+const db = require("knex")({
+  client: "sqlite3",
+  connection: {
+    filename: './database.sqlite'
+  }
 });
-const testDataBase = () => db.serialize(function () {
-  db.each("SELECT * FROM Products", function (err, row) {
-    console.log(row);
-  });
-});
+
+const testDataBase = () => {
+  let result = db.select("name").from("Products")
+  result.then(function (rows) {
+    console.log("resultSent", rows);
+  })
+};
 
 const closeDataBase = db.close;
 
 module.exports = {
   db,
   testDataBase,
-  createTestDataBase,
   closeDataBase
 }
